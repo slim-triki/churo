48bd8bb71dfb852eefbf17ce9129cd2800002e7f
churo <slimtriki69@gmail.com>
2021-07-29 11:00:02 +0200

    protected route in app.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import { useState } from 'react';
-import { BrowserRouter as Router, Route ,Link} from 'react-router-dom';
+/*import { useState } from 'react';*/
+import { BrowserRouter as Router, Route ,Redirect} from 'react-router-dom';
 import './App.css';
-import ProtectedRoute from './components/common/ProtectedRoute';
+/*import ProtectedRoute from './components/common/ProtectedRoute';*/
 import Page from './components/pages/page';
 
 /*var objpeople =[
@@ -30,7 +30,7 @@ const handleSubmit = (e) => {
 };*/
 
 function App() {
-  const [isAuth, setIsAuth] = useState(false);
+  /*const [isAuth, setIsAuth] = useState(false);*/
   return (
     <Router>
       <Route path="/" exact>
@@ -49,14 +49,14 @@ function App() {
         <input type="password"  id="pass"    />
 
       </section>
-        <button type="submit" onClick={Auth} >Login</button>
-        <Link to="/page" onClick={() =>{setIsAuth(true);}}>go</Link>
+        <button type="submit" onClick={ProtectedRoute} >Login</button>
+      
       </section>
      </div>
      
      </Route>
      
-     <ProtectedRoute path="/Page" component={Page} isAuth={isAuth}  />
+     <ProtectedRoute path="/Page" component={Page}   />
      </Router>
   );
 }
@@ -72,25 +72,50 @@ var objpeople =[
       password:"k"
   }
   ]
-function Auth(){
-            
-  
-  var i;
-  var username = document.getElementById("user").value;
-  var password = document.getElementById("pass").value;
 
-  for (i=0; i<objpeople.length;i++){
-
-      if (username === objpeople[i].username && password === objpeople[i].password )
-          {
-              
-             console.log(username + password +" is logged !"); 
-             return
-          }}
-      
-          console.log (username + password  +"incorrect username or password")
-}
         
 
 
 export default App;
+function ProtectedRoute ({  component: Component, ...Rest  } ) {
+ /* var i;*/
+  var username = document.getElementById("user").value;
+  var password = document.getElementById("pass").value;
+  for (let i=0; i<objpeople.length;i++){
+  return (
+      <Route {...Rest} render={(props)=>{
+          if (username === objpeople[i].username && password === objpeople[i].password ){
+              return <Component/>
+          } else {
+              return (
+                  <Redirect to={{pathname: "/", state: {from: props.location}}}/>
+              );
+          }
+      }
+
+      }/>
+          
+      
+  )
+}}
+
+
+/*function Auth(){
+       
+
+var i;
+var username = document.getElementById("user").value;
+var password = document.getElementById("pass").value;
+
+for (i=0; i<objpeople.length;i++){
+
+   if (username === objpeople[i].username && password === objpeople[i].password )
+       {
+           
+          console.log(username + password +" is logged !"); 
+          return(true)
+       }}
+   
+       console.log (username + password  +"incorrect username or password")
+}
+ <Link to="/page" onClick={() =>{setIsAuth(true);}}>go</Link>*/   
